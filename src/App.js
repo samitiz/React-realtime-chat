3f03cc3d02112abbb8b0337fa9a812ddf22b6b02
samitiz <usamasam687@gmail.com>
2021-07-07 14:19:44 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="ce23309e-b52a-482f-87de-94c8455fb875"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => < ChatFeed {...chatAppProps} />}
+        />
+    )
+}
+
+export default App
